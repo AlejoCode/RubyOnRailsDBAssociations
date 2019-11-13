7e56b9083f2859592fd1c9fc22520910a073cffd
gonjavi <javiercaliescali@gmail.com>
2019-11-13 16:22:50 -0500

    checking linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
+  end
 end
 
   def update
